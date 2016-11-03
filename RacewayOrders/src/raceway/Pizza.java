8f7a4ef8b2cd84dcb0368879ed6514fca91e1231
connor <connor@CBPC>
2016-11-03 23:39:16 +1300

    Pizza class returns list of pizzas to get chosen

--- RacewayOrders/src/raceway/Pizza.java
+++ RacewayOrders/src/raceway/Pizza.java
@@ -1,15 +1,62 @@
 package raceway;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
 
 public class Pizza implements Item{
-	public static List <String>names;
-	
-	public Pizza(){
-		
+
+	public static List <Pizza> items;
+
+	private String name;
+	private double cost;
+
+	/**
+	 * create a pizza class with a name and a cost to it
+	 * @param name
+	 * @param cost
+	 */
+	public Pizza(String name, double cost){
+		this.name = name;
+		this.cost = cost;
 	}
-		
+
 	public double getCost(){
-		return 0;
+		return cost;
 	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public static List <Pizza> addToList(){
+		items = new ArrayList<>();
+		items.add(new Pizza("Lust",16));
+		items.add(new Pizza("Greed",16));
+		items.add(new Pizza("Gluttony",16));
+		items.add(new Pizza("Pandy",18.5));
+		items.add(new Pizza("Envy",16));
+		items.add(new Pizza("Grimm",18.5));
+		items.add(new Pizza("Mordor",18.5));
+		items.add(new Pizza("Pride",16));
+		items.add(new Pizza("Lust",16));
+		items.add(new Pizza("Greed",16));
+		items.add(new Pizza("Gluttony",16));
+		items.add(new Pizza("Pandy",18.5));
+		items.add(new Pizza("Envy",16));
+		items.add(new Pizza("Grimm",18.5));
+		items.add(new Pizza("Mordor",18.5));
+		items.add(new Pizza("Pride",16));
+		items.add(new Pizza("Lust",16));
+		items.add(new Pizza("Greed",16));
+		items.add(new Pizza("Gluttony",16));
+		items.add(new Pizza("Pandy",18.5));
+		items.add(new Pizza("Envy",16));
+		items.add(new Pizza("Grimm",18.5));
+		items.add(new Pizza("Mordor",18.5));
+		items.add(new Pizza("Pride",16));
+		return items;
+	}
+
 }
